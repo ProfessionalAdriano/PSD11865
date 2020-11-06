17eec647c6515169c30dd71093ccd9bd5cbece19
Adriano Lima <adrianolima095@gmail.com>
2020-11-06 20:53:13 -0300

    Alterado o nome dos parametros - estava gerando inconsistencia com as variaveis - Dev Renato Davi

--- 04-Procedure_calcula.sql
+++ 04-Procedure_calcula.sql
@@ -13,37 +13,47 @@ SELECT TO_CHAR(SYSDATE,'DD/MM/YYYY HH24:MI:SS') DATA FROM DUAL;
   Autor: Adriano/Sarah/Renato
   Data: 04/11/2020
 */ 
-
-CREATE OR REPLACE PROCEDURE OWN_FUNCESP.PRE_PRC_EXTRATOCALCULA(COD_EMPRESA NUMBER,
-                                                               DCR_PLANO   VARCHAR2,
-                                                               DTA_MOV     DATE)IS
+CREATE OR REPLACE PROCEDURE OWN_FUNCESP.PRE_PRC_EXTRATOCALCULA(P_COD_EMPRESA FC_PRE_TBL_BASE_EXTRAT_CTB.COD_EMPRS%TYPE ,
+                                                               P_DCR_PLANO   FC_PRE_TBL_BASE_EXTRAT_CTB.DCR_PLANO%TYPE,
+                                                               P_DTA_MOV     FC_PRE_TBL_BASE_EXTRAT_CTB.DTA_FIM_EXTR%TYPE )IS
+L_DT_FIM ATT.FC_PRE_TBL_BASE_EXTRAT_CTB.DTA_FIM_EXTR%TYPE;
 
 BEGIN
-
-  DECLARE
+  -- L_DT_FIM := '';
+  --DECLARE
 
      -- VARIAVEL DECLARADA PARA GUARDAR A MAIOR DATA DA PUBLICACAO DO EXTRATO.
-     VDATA_FIM VARCHAR2(200);
+     --VDATA_FIM DATE;
 
     BEGIN
+    -- IF ( DTA_MOV IS NOT NULL ) THEN
+      
+      IF P_COD_EMPRESA = 40 AND UPPER(P_DCR_PLANO) = UPPER('PSAP/Eletropaulo') THEN
 
-      SELECT MAX(FP.DTA_FIM_EXTR)INTO VDATA_FIM FROM ATT.FC_PRE_TBL_BASE_EXTRAT_CTB FP WHERE FP.COD_EMPRS = COD_EMPRESA AND FP.DCR_PLANO = DCR_PLANO;
-      IF COD_EMPRESA = 40 AND UPPER(DCR_PLANO) = 'PSAP/ELETROPAULO' THEN
-
-
+      SELECT MAX(TO_DATE(FP.DTA_FIM_EXTR,'DD/MM/RRRR'))
+      --SELECT FP.DTA_FIM_EXTR
+      INTO L_DT_FIM
+      FROM ATT.FC_PRE_TBL_BASE_EXTRAT_CTB FP
+      WHERE FP.COD_EMPRS = P_COD_EMPRESA
+       AND UPPER(FP.DCR_PLANO) = UPPER(P_DCR_PLANO)
+       AND NUM_RGTRO_EMPRG = '0002078236-9';
+       
+      
+       DBMS_OUTPUT.PUT_LINE('L_DT_FIM: ' || TO_CHAR(L_DT_FIM));
+       
           INSERT INTO OWN_FUNCESP.PRE_TBL_TEMPEXTRAT_CTB(COD_EMPRESA, NUM_REGISTRO, DCR_PLANO, NUM_MATR,COD_PLANO)
             SELECT X.COD_EMPRS, X.NUM_RGTRO_EMPRG, X.DCR_PLANO, Y.NUM_MATR_PARTF,19
               FROM ATT.FC_PRE_TBL_BASE_EXTRAT_CTB X
              INNER JOIN ATT.PARTICIPANTE_FSS Y ON Y.COD_EMPRS = X.COD_EMPRS
                                               AND Y.NUM_RGTRO_EMPRG =
                                                   TO_NUMBER(SUBSTR(X.NUM_RGTRO_EMPRG,1,LENGTH(X.NUM_RGTRO_EMPRG) - 2))
-             WHERE X.COD_EMPRS = COD_EMPRESA
-               AND X.DCR_PLANO = DCR_PLANO
+             WHERE X.COD_EMPRS = P_COD_EMPRESA
+               AND X.DCR_PLANO = P_DCR_PLANO
              GROUP BY X.COD_EMPRS,X.NUM_RGTRO_EMPRG,X.DCR_PLANO,Y.NUM_MATR_PARTF;
 
 
           --INICIO DAS CONSISTENCIAS DE DADOS
-          IF DTA_MOV IS NULL THEN
+          IF P_DTA_MOV IS NULL THEN
               UPDATE OWN_FUNCESP.PRE_TBL_TEMPEXTRAT_CTB E
                  SET E.DTA_FIM = (SELECT MAX(DTA_FIM_EXTR)
                                     FROM ATT.FC_PRE_TBL_BASE_EXTRAT_CTB
@@ -55,10 +65,11 @@ BEGIN
 
           ELSE
                UPDATE OWN_FUNCESP.PRE_TBL_TEMPEXTRAT_CTB E
-                 SET E.DTA_FIM = DTA_MOV
+                 SET E.DTA_FIM = P_DTA_MOV
                WHERE E.DTA_FIM IS NULL;
 
           END IF;
+
 
           UPDATE OWN_FUNCESP.PRE_TBL_TEMPEXTRAT_CTB E
            SET E.VLR_BENEF_INTE = (SELECT NVL(MAX(VLR_BENEF1_HTBNF),0)
@@ -121,8 +132,8 @@ BEGIN
                                                                   AND D.NUM_CTFSS      = A.NUM_CTFSS
                                                                   AND D.COD_UM         = A.COD_UM
                                                                   --- Prioridade, parametro date informado, ou maior dta da publicacao do extrato:
-                                                                  AND D.ANOMES_MOVIM_SDCTPR = ANY (TO_NUMBER(TO_CHAR(TRUNC(DTA_MOV,'YYYYMM'))), TO_NUMBER(TO_CHAR(TRUNC(VDATA_FIM,'YYYYMM'))))
-                                                                                                                                         
+                                                                  AND D.ANOMES_MOVIM_SDCTPR = TO_NUMBER(TRUNC(TO_CHAR(P_DTA_MOV,'YYYYMM')))
+
 
                                                               )
 
@@ -135,8 +146,8 @@ BEGIN
                                   WHERE A.COD_UM = 248
                                    AND A.DAT_CDIAUM = (SELECT MAX(DAT_CDIAUM)
                                                         FROM COTACAO_DIA_UM
-                                                         WHERE COD_UM = A.COD_UM                                                         
-                                                         AND DAT_CDIAUM = ANY (TO_DATE(TRUNC(DTA_MOV)), TO_DATE(TRUNC(VDATA_FIM)))
+                                                         WHERE COD_UM = A.COD_UM
+                                                         AND DAT_CDIAUM = TO_DATE(TRUNC(P_DTA_MOV))
                                                        )
                                )
 
@@ -163,7 +174,57 @@ BEGIN
       COMMIT;
       DBMS_OUTPUT.PUT_LINE('TEMPORARIA PREENCHIDA');
 
+/*      ELSE
 
+      --DBMS_OUTPUT.PUT_LINE('TESTE 2');
+               INSERT INTO OWN_FUNCESP.PRE_TBL_TEMPEXTRAT_CTB(COD_EMPRESA, NUM_REGISTRO, DCR_PLANO, NUM_MATR,COD_PLANO)
+            SELECT X.COD_EMPRS, X.NUM_RGTRO_EMPRG, X.DCR_PLANO, Y.NUM_MATR_PARTF,19
+              FROM ATT.FC_PRE_TBL_BASE_EXTRAT_CTB X
+             INNER JOIN ATT.PARTICIPANTE_FSS Y ON Y.COD_EMPRS = X.COD_EMPRS
+                                              AND Y.NUM_RGTRO_EMPRG =
+                                                  TO_NUMBER(SUBSTR(X.NUM_RGTRO_EMPRG,1,LENGTH(X.NUM_RGTRO_EMPRG) - 2))
+             WHERE X.COD_EMPRS = 40 --COD_EMPRESA
+               AND X.DCR_PLANO = 'PSAP/Eletropaulo' --DCR_PLANO
+             GROUP BY X.COD_EMPRS,X.NUM_RGTRO_EMPRG,X.DCR_PLANO,Y.NUM_MATR_PARTF;
+
+
+          --INICIO DAS CONSISTENCIAS DE DADOS
+          IF DTA_MOV IS NULL THEN
+              UPDATE OWN_FUNCESP.PRE_TBL_TEMPEXTRAT_CTB E
+                 SET E.DTA_FIM = (SELECT MAX(DTA_FIM_EXTR)
+                                    FROM ATT.FC_PRE_TBL_BASE_EXTRAT_CTB
+                                   WHERE COD_EMPRS = 40 --E.COD_EMPRESA
+                                     AND DCR_PLANO = 'PSAP/Eletropaulo' --E.DCR_PLANO
+                                     AND NUM_RGTRO_EMPRG = E.NUM_REGISTRO
+                                  )
+               WHERE E.DTA_FIM IS NULL;
+
+          ELSE
+               UPDATE OWN_FUNCESP.PRE_TBL_TEMPEXTRAT_CTB E
+                 SET E.DTA_FIM = DTA_MOV
+               WHERE E.DTA_FIM IS NULL;
+
+          END IF;
+
+
+          UPDATE OWN_FUNCESP.PRE_TBL_TEMPEXTRAT_CTB E
+           SET E.VLR_BENEF_INTE = (SELECT NVL(MAX(VLR_BENEF1_HTBNF),0)
+                                   FROM HIST_VALOR_BNF
+                                   WHERE NUM_MATR_PARTF = NUM_MATR
+                                    AND COD_NATBNF = 4
+                                    AND TO_CHAR(DAT_INIVG_HTBNF,'YYYYMM') = TO_CHAR(E.DTA_FIM,'YYYYMM')
+                                  )
+         WHERE E.VLR_BENEF_INTE = 0;
+         COMMIT;
+
+
+
+
+
+
+      END IF;*/
     END;
 END;
+
+
 
